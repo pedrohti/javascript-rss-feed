e1026405d085491d98a8351d03610b90c39d9f4e
PedroHMoraes <pedroh.ti@hotmail.com>
2020-03-10 19:00:58 -0300

    :sparkles: Changed UL for DataTable

--- rss.js
+++ rss.js
@@ -2,36 +2,49 @@
 const cors = 'https://cors-anywhere.herokuapp.com/'
 // const cors = 'http://crossorigin.me/'
 
-const baseURL = 'PUT_URL_HERE'
+const baseURL = 'http://mantis.starlinetecnologia.com.br/issues_rss.php?username=unifoa.pedro&key=IFVNAsk03YU3kRf7wbh6ts6ZMGggWVRUzPIQfjRk3iHq3LrkeVcU9l0Ck_7QeQo2AOutUdVPoHqTB9x5XXYO'
+
 
 fetch(cors + baseURL)
     .then(response => response.text())
     .then(str => new DOMParser().parseFromString(str, 'text/xml'))
-    .then(data => {
-        console.log(data)
+    .then(dados => {
+        moment.locale('pt-BR');
         let html = ``
-        const items = data.querySelectorAll("item")
+        
+        const items = dados.querySelectorAll("item")
 
+        var chamado, titulo, descricao, data, categoria
+
+        var array = []
+        
         items.forEach(item => {
-            // var chamado = item.querySelector("title").innerHTML.replace(/[a-zA-Z\s\W]|:/g,"")
-            var chamado = item.querySelector("title").innerHTML.slice(0, 7)
-            var titulo = item.querySelector("title").innerHTML.replace(/[0-9]|:/g,"")
-            
-            var descricao = item.querySelector("description").innerHTML.replace(/[|<|!|[|C|D|A|T|A|]|>|]|/g, '')
-            var data = item.querySelector("pubDate").innerHTML
-            var categoria = item.querySelector("category").innerHTML
-            
+            chamado = item.querySelector("title").innerHTML.slice(0, 7)
+            titulo = item.querySelector("title").innerHTML.replace(/[0-9]|:/g,"")            
+            descricao = item.querySelector("description").innerHTML.replace(/[|<|!|[|C|D|A|T|A|]|>|]|/g, '')
+            data = moment(item.querySelector("pubDate").innerHTML).format('L')
+            categoria = item.querySelector("category").innerHTML
+
             // Simple example code, the exibition on Index can be changed.
-            
-            html += `
-                <ul>
-                    <li>${chamado}</li>
-                    <li>${titulo}</li>
-                    <li>${descricao}</li>
-                    <li>${data}</li>
-                    <li>${categoria}</li>
-                </ul>
-            `
+
+            array.push([chamado, descricao, data])
+
+
+            // html += `
+            //         <li><b>${chamado}</b> - ${descricao} (${data})</li>
+            //     </ul>
+            // `
+
         })
-        document.body.insertAdjacentHTML("beforeend", html)
+
+        $('#tblChamados').DataTable({
+            data: array,
+            columns: [
+              { title: 'Chamado' },
+              { title: 'Descrição' },
+              { title: 'Data' }
+            ]
+          })
+        console.log(array)
+        // document.body.insertAdjacentHTML("beforeend", html)
     })
