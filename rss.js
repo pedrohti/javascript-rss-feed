a18f24ec6a13075abc5bcfa9afe41b3d8e30268b
PedroHMoraes <pedroh.ti@hotmail.com>
2020-03-13 18:44:26 -0300

    Update rss.js

--- rss.js
+++ rss.js
@@ -2,50 +2,57 @@
 const cors = 'https://cors-anywhere.herokuapp.com/'
 // const cors = 'http://crossorigin.me/'
 
-const baseURL = 'URL_HERE'
+const baseURL = 'URL'
 
+fetch(cors + baseURL, {headers: {Authentication: `${token}`}})
+	.then(response => response.text())
+	.then(str => new DOMParser().parseFromString(str, 'text/xml'))
+	.then(dados => {
+		moment.locale('pt-BR');
+		let html = ``
 
-fetch(cors + baseURL)
-    .then(response => response.text())
-    .then(str => new DOMParser().parseFromString(str, 'text/xml'))
-    .then(dados => {
-        moment.locale('pt-BR');
-        let html = ``
-        
-        const items = dados.querySelectorAll("item")
+		const items = dados.querySelectorAll("item")
 
-        var chamado, titulo, descricao, data, categoria
+		var chamado, titulo, descricao, data, categoria
 
-        var array = []
-        
-        items.forEach(item => {
-            chamado = item.querySelector("title").innerHTML.slice(0, 7)
-            titulo = item.querySelector("title").innerHTML.replace(/[0-9]|:/g,"")            
-            descricao = item.querySelector("description").innerHTML.replace(/[|<|!|[|C|D|A|T|A|]|>|]|/g, '')
-            data = moment(item.querySelector("pubDate").innerHTML).format('L')
-            categoria = item.querySelector("category").innerHTML
+		var array = []
 
-            // Simple example code, the exibition on Index can be changed.
+		items.forEach(item => {
+			chamado = item.querySelector("title").innerHTML.slice(0, 7)
+			titulo = item.querySelector("title").innerHTML.replace(/[0-9]|:/g, "")
+			descricao = item.querySelector("description").innerHTML.replace(/[|<|!|[|C|D|A|T|A|]|>|]|/g, '')
+			data = moment(item.querySelector("pubDate").innerHTML).format('L')
+			categoria = item.querySelector("category").innerHTML
+			// link = item.querySelector("link").innerHTML
 
-            array.push([chamado, descricao, data])
+			// Simple example code, the exibition on Index can be changed.
 
+			array.push([chamado, descricao, data])
 
-            // html += `
-            //         <li><b>${chamado}</b> - ${descricao} (${data})</li>
-            //     </ul>
-            // `
+			// html += `
+			//         <li><a href="link?id=${chamado}" target='_blank'>${chamado}</a> - ${descricao} (${data})</li>
+			//     </ul>
+			// `
 
-        })
+		})
 
-        $('#tblChamados').DataTable({
-            data: array,
-            order: [[2, 'desc']],
-            columns: [
-              { title: 'Chamado' },
-              { title: 'Descrição' },
-              { title: 'Data' }
-            ]
-          })
-        console.log(array)
-        // document.body.insertAdjacentHTML("beforeend", html)
-    })
+		$('#tblChamados').DataTable({
+			data: array,
+			order: [[2, 'desc']],
+			columns: [
+				{ title: 'Chamado' },
+				{ title: 'Descrição' },
+				{ title: 'Data' },
+			]
+		})
+		
+		var table = $('#tblChamados').DataTable();
+
+		$('#tblChamados tbody').on('click', 'tr', function () {
+			var id = table.row(this).data();
+			window.open(`?id=${id[0]}`, '_blank')
+		} );
+
+		console.log(array)
+		// document.body.insertAdjacentHTML("beforeend", html)
+	})
